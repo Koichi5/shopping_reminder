7b2b59af748b9739d4caccd5a33d244ce4d4b508
Koichi5 <koichi20021217@gmail.com>
2023-06-10 22:39:35 +0900

    edit shopping item view

--- ShoppingReminderApp.swift
+++ ShoppingReminderApp.swift
@@ -28,6 +28,7 @@ class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDele
 
 @main
 struct ShoppingReminderApp: App {
+    @StateObject var vibrationHelper = VibrationHelper()
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
--- Views/ShoppingItemView.swift
+++ Views/ShoppingItemView.swift
@@ -10,14 +10,13 @@ import SwiftUI
 struct ShoppingItemView: View {
     @ObservedObject private var shoppingItemRepository = ShoppingItemRepository()
     @State private var shoppingItemList = ShoppingItemRepository().shoppingItemList
-    @State private var categories: [String] = []
-
+    
     var body: some View {
-ScrollView (showsIndicators: false){
+        ScrollView (showsIndicators: false){
             ZStack {
-                shoppingItemRepository.shoppingItemList.isEmpty || shoppingItemRepository.shoppingItemList.count == 0
+                shoppingItemList.isEmpty || shoppingItemList.count == 0
                 ? nil
-                : 
+                :
                 VStack {
                     ForEach (shoppingItemRepository.shoppingItemCategoryList, id: \.hashValue) { shoppingItemCategory in
                         ZStack {
@@ -28,7 +27,7 @@ ScrollView (showsIndicators: false){
                                 Text(shoppingItemCategory)
                                     .frame(maxWidth: .infinity, alignment: .leading)
                                     .padding()
-                                ForEach (shoppingItemRepository.shoppingItemList) { shoppingItem in
+                                ForEach (shoppingItemList) { shoppingItem in
                                     shoppingItem.category.name == shoppingItemCategory
                                     ? ShoppingItemComponent(shoppingItem: shoppingItem).padding(.horizontal)
                                     : nil
@@ -41,8 +40,8 @@ ScrollView (showsIndicators: false){
                 .task {
                     do {
                         try await shoppingItemRepository.addUserSnapshotListener()
-                        let shoppingItemDict = Dictionary(grouping: shoppingItemList.filter({ $0.category.name != nil })) { $0.category.name }
-                        print("shoppingItemDict in shopping item view: \(shoppingItemDict)")
+//                        let shoppingItemDict = Dictionary(grouping: shoppingItemList.filter({ $0.category.name != nil })) { $0.category.name }
+//                        print("shoppingItemDict in shopping item view: \(shoppingItemDict)")
                         updateCategories()
                     } catch {
                         print(error)
@@ -52,77 +51,9 @@ ScrollView (showsIndicators: false){
     }
     private func updateCategories() {
         var categories = Set<String>()
-        for item in shoppingItemRepository.shoppingItemList {
+        for item in shoppingItemList {
             categories.insert(item.category.name)
         }
-        self.categories = Array(categories)
+//        self.categories = Array(categories)
     }
 }
-
-//import SwiftUI
-//
-//struct ShoppingItemView: View {
-//    @ObservedObject private var shoppingItemRepository = ShoppingItemRepository()
-//    @State private var shoppingItemList = ShoppingItemRepository().shoppingItemList
-//    @State private var categories: [String] = []
-//
-//    var body: some View {
-//        if shoppingItemRepository.shoppingItemList.isEmpty || shoppingItemRepository.shoppingItemList.count == 0 {
-//            Text("追加しよう")
-//        } else {
-//            VStack {
-//                ScrollView(showsIndicators: false) {
-//                    ZStack {
-//                        VStack {
-//                            ForEach(shoppingItemRepository.shoppingItemCategoryList, id: \.hashValue) { shoppingItemCategory in
-//                                ZStack {
-//                                    Color.white
-//                                        .cornerRadius(10)
-//                                        .shadow(color: .gray.opacity(0.7), radius: 2, x: 5, y: 5)
-//                                    VStack {
-//                                        Text(shoppingItemCategory)
-//                                            .frame(maxWidth: .infinity, alignment: .leading)
-//                                            .padding()
-//                                        ForEach(shoppingItemRepository.shoppingItemList) { shoppingItem in
-//                                            if shoppingItem.category.name == shoppingItemCategory {
-//                                                ShoppingItemComponent(shoppingItem: shoppingItem).padding(.horizontal)
-//                                            } else {
-//                                                EmptyView()
-//                                            }
-//                                        }.padding(.vertical)
-//                                    }
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            .padding(.trailing, 5)
-//            .task {
-//                do {
-//                    try await shoppingItemRepository.addUserSnapshotListener()
-//                    let shoppingItemDict = Dictionary(grouping: shoppingItemList.filter({ $0.category.name != nil })) { $0.category.name }
-//                    print("shoppingItemDict in shopping item view: \(shoppingItemDict)")
-//                    updateCategories()
-//                } catch {
-//                    print(error)
-//                }
-//            }
-//        }
-//    }
-//
-//    private func updateCategories() {
-//        var categories = Set<String>()
-//        for item in shoppingItemRepository.shoppingItemList {
-//            categories.insert(item.category.name)
-//        }
-//        self.categories = Array(categories)
-//    }
-//}
-//
-//
-//struct ShoppingItemView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ShoppingItemView()
-//    }
-//}

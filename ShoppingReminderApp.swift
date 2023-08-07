1cd39443d67b8e5fb01ad34e8367444900e41930
Koichi5 <koichi20021217@gmail.com>
2023-08-08 00:46:14 +0900

    edit location views

--- ShoppingReminderApp.swift
+++ ShoppingReminderApp.swift
@@ -29,19 +29,20 @@ struct ShoppingReminderApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            if Auth.auth().currentUser != nil {
-                HomeView()
-            } else {
-                EntryAuthView()
-                    .onOpenURL{ url in
-                        GIDSignIn.sharedInstance.handle(url)
-                    }
-                    .onAppear{
-                        GIDSignIn.sharedInstance.restorePreviousSignIn{ user,error in
-                            print("logged in as: \(user?.userID)")
-                        }
-                    }
-            }
+            LocationView()
+//            if Auth.auth().currentUser != nil {
+//                HomeView()
+//            } else {
+//                EntryAuthView()
+//                    .onOpenURL{ url in
+//                        GIDSignIn.sharedInstance.handle(url)
+//                    }
+//                    .onAppear{
+//                        GIDSignIn.sharedInstance.restorePreviousSignIn{ user,error in
+//                            print("logged in as: \(user?.userID)")
+//                        }
+//                    }
+//            }
         }
     }
 }
